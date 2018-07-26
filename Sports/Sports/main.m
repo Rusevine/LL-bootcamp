351239aa472821ed23852589c2934a70c84ae1c5
Wiljay <wiljay.flores@gmail.com>
2018-07-26 04:53:22 -0400

    Created a NSArray called Sports

--- Sports/Sports/main.m
+++ Sports/Sports/main.m
@@ -9,9 +9,7 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    NSArray *sports = @[@"Basketball",@"Curling",@"Baseball",@"Volleyball"];
+    NSLog(@"%@",sports[1]);
     return 0;
 }
