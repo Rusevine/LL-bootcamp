93b1ebdcee4e7545fa01d009e0ee919277dd405c
Wiljay <wiljay.flores@gmail.com>
2018-07-28 14:47:16 -0400

    Created a second Dog object, added a pet property to Person and I assigned both Dogs to pet

--- customClasses/customClasses/main.m
+++ customClasses/customClasses/main.m
@@ -13,11 +13,17 @@
 int main(int argc, const char * argv[]) {
     Person *person1 = [[Person alloc] init];
     Dog *dog1 = [[Dog alloc] init];
+    Dog *dog2 = [[Dog alloc] init];
     
     person1.name = @"Wiljay";
     dog1.name = @"Teddy";
+    dog2.name = @"Coco";
+    
+    person1.pet = dog1;
+    person1.pet = dog2;
     
     NSLog(@"%@", person1.name);
+    NSLog(@"%@", person1.pet.name);
     
     return 0;
 }
